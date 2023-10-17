50081500a8ffdc9f91b9c39f8005b88aeb735bc6
Amogo Solomon <amogoagaba@gmail.com>
2023-10-17 16:16:50 +0100

    pushing first update

---
+++ src/components/data/dummydata.js
@@ -0,0 +1,90 @@
+import { Settings, CropRotate, ViewInAr, PieChart, Code, BarChart, 
+  CloudOutlined, FavoriteBorder, Public, PersonOutlined, AddLocationAltOutlined,
+   PhoneIphone, EmailOutlined, Facebook, Twitter, Instagram, YouTube, LinkedIn, GitHub } from "@mui/icons-material"
+
+   export const navlink = [
+    {
+        url: "/",
+        text: "Home",
+    },
+    {
+        url: "/about",
+        text: "About Us",
+    },
+    {
+        url: "/services",
+        text: "Services",
+    },
+    {
+        url: "/blog",
+        text: "Blog",
+    },
+    {
+        url: "/contact",
+        text: "Contact Us",
+    },
+   ]
+   export const home = [
+    {
+
+    },
+   ]
+   export const about = [
+    {
+
+    },
+   ]
+   export const services = [
+    {
+        
+    },
+   ]
+   export const blog = [
+    {
+        id: 1,
+        title: "",
+        date: "october 1st, 23",
+        author: "Barr Francis Ochi",
+        content: (
+            <div>
+                <p>
+
+                </p>
+            </div>
+        ),
+        summary:
+            "..",
+        cover: "",
+    },
+   ]
+   export const contact = [
+    {
+        icon: <AddLocationAltOutlined />,
+        text1:"address",
+        text2: "state, Country",
+    },
+    {
+        icon: <PhoneIphone />,
+        text1: "+234 7084857763",
+        text2: "+234 9061214452",
+    },
+    {
+        icon: <EmailOutlined />,
+        text1: "illedufrancis@gmail.com",
+        text2: "",
+    },
+   ]
+   export const social = [
+    {
+        icon:<YouTube />,
+        url: "",
+    },
+    {
+        icon: <LinkedIn />,
+        url: "",
+    },
+    {
+        icon: <Facebook />,
+        url: "",
+    },
+   ]
