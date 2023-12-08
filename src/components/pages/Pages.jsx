bc4fc2bccc6da60469a6421f53f829dd3eed597c
Amogo Solomon <amogoagaba@gmail.com>
2023-12-08 23:04:54 +0100

    updating whatsApp button

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Navbar } from "../common/Navbar";
 import { Home } from "../home/Home";
 import { About } from "./About";
 import { Services } from "./Services";
-import  Testimonials   from "./Testimonials";
+import Testimonials from "./Testimonials";
 import { Blog } from "./Blog";
 import { Contact } from "./Contact";
 import { Footer } from "./Footer";
-import { Link, Element, Events, animateScroll as scroll, scrollSpy } from "react-scroll"; // Import Link and Element from react-scroll
-import ScrollToTop from "react-scroll-to-top"; // Import ScrollToTop component
+import { Link, Element } from "react-scroll";
+import ScrollToTop from "react-scroll-to-top";
+import { WhatsApp } from '@mui/icons-material';
 
 export const Pages = () => {
-  // Function to scroll to the top when the button is clicked
+  const [showWhatsApp, setShowWhatsApp] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentPosition = window.pageYOffset;
+
+      if (currentPosition > window.innerHeight) {
+        setShowWhatsApp(false);
+      } else {
+        setShowWhatsApp(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   const scrollToTop = () => {
-    scroll.scrollToTop();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
+  const whatsappNumber = '+2347084857763';
+
+  const handleWhatsAppClick = () => {
+    window.open(`https://wa.me/${whatsappNumber}`, '_blank');
   };
 
   return (
@@ -28,20 +54,28 @@ export const Pages = () => {
           <Route path="/testimonials" element={<Testimonials />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/blog" element={<Blog />} />
-          
         </Routes>
         <Footer />
       </Router>
 
+      {/* WhatsApp button with icon */}
+      {showWhatsApp && (
+        <button
+          className="fixed bottom-20 right-2 bg-green-500 hover:bg-green-600 text-white md:text-sm text-sm md:px-4 px-2 md:py-2  py-1 rounded-md flex items-center gap-2  block sm:block md:block xsm:block font-normal"
+          onClick={handleWhatsAppClick}
+        >
+          <WhatsApp fontSize="small" /> How may we help you?
+        </button>
+      )}
+
       {/* Scroll-to-top button */}
       <Link
-        to="top" // Specify the ID of the element you want to scroll to (e.g., "top")
-        smooth={true} // Enable smooth scrolling
-        duration={500} // Duration of the scrolling animation in milliseconds
-        className="scroll-to-top fixed bottom-6 right-6 bg-primaryColor text-white p-2 rounded-full cursor-pointer transition-opacity hover:opacity-80"
-      >
-       
-      </Link>
+        to="top"
+        smooth={true}
+        duration={500}
+        className="scroll-to-top fixed bottom-0.5 right-1 bg-primaryColor text-white p-2 rounded-full cursor-pointer transition-opacity hover:opacity-80"
+        onClick={scrollToTop}
+      ></Link>
 
       {/* Add the Element component with an ID corresponding to the Link */}
       <Element name="top" className="element" />
@@ -49,10 +83,14 @@ export const Pages = () => {
       {/* Scroll-to-top button from react-scroll-to-top */}
       <ScrollToTop
         smooth
-        component={<div className="fixed bottom-6 right-6 bg-primaryColor text-white p-2 rounded-full cursor-pointer transition-opacity hover:opacity-80">^</div>}
+        component={
+          <div className="fixed bottom-0.5 right-2 bg-primaryColor text-white p-2 rounded-full cursor-pointer transition-opacity hover:opacity-80">^</div>
+        }
         topPosition={100}
         style={{ zIndex: 999 }}
       />
     </>
   );
 };
+
+export default Pages;
